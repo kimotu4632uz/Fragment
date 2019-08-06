aa813da3d4cebcbea2067a724e42ab68b75a3481
kimotu4632uz <kimotu.phone@gmail.com>
2019-08-06 17:35:22 +0900

    finished writing termux-url-opener

---
+++ termux/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pkg install -y libjpeg-turbo ffmpeg-dev libxml2 libxslt
+ln -s $PREFIX/include/{libxml2/libxml,libxml}
+pip install requests mutagen Pillow img2pdf bs4 ffmpeg gmusicapi
+python -c "from gmusicapi import Musicmanager; Musicmanager().perform_oauth(storage_filepath='/data/data/com.termux/files/home/.oauth.cred')"
