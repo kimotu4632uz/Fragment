6e45c68e137541cdaee244cace63495bbc049490
kimotu4632uz <kimotu.phone@gmail.com>
2019-08-14 20:04:09 +0900

    update termux files

--- termux/setup.sh
+++ termux/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-pkg install -y rclone libjpeg-turbo ffmpeg-dev libxml2 libxslt
+# libxml2 libxslt : for bs4, libjpeg-turbo : for Pillow
+pkg install -y rclone ffmpeg-dev libxml2 libxslt libjpeg-turbo
 ln -s $PREFIX/include/{libxml2/libxml,libxml}
-pip install requests mutagen Pillow img2pdf bs4 ffmpeg gmusicapi
+pip install requests mutagen img2pdf bs4 ffmpeg gmusicapi Pillow
 python -c "from gmusicapi import Musicmanager; Musicmanager().perform_oauth(storage_filepath='/data/data/com.termux/files/home/.oauth.cred')"
+rclone config create upload_pic drive env_auth true
