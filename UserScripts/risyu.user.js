687b8dbb01f6af8c51bf0c5cf16601018e62af93
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2020-08-20 09:35:01 +0900

    update risyu.user.js

--- UserScripts/risyu.user.js
+++ UserScripts/risyu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Risyu to .ics
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  saidai riyuuu page to icalender format file
 // @author       kimotu
 // @match        https://risyu.saitama-u.ac.jp/portal/StudentApp/Regist/RegistList.aspx
@@ -32,14 +32,22 @@
 
     for (var i = 1; i <= 7; i++) {
         for (let day of days) {
-            let e = document.getElementById(`ctl00_phContents_rrMain_ttTable_lct${day}${i}_ctl00_lblSbjName`);
+            for (let sbj of document.getElementById(`ctl00_phContents_rrMain_ttTable_td${day}${i}`).children) {
+                if (sbj.tagName === 'DIV') {
+                    let ct = sbj.id.split('_')[5];
 
-            if (e.children.length > 0) {
-                subjects.push({ "name": e.firstElementChild.innerHTML.split('<br>')[0], "day": days.indexOf(day) + 1, "time": i });
+                    let sbjname = sbj.getElementById(`ctl00_phContents_rrMain_ttTable_lct${day}${i}_${ct}_lblSbjName`);
+                    let teacher = sbj.getElementById(`ctl00_phContents_rrMain_ttTable_lct${day}${i}_${ct}_lblStaffName`)
+
+                    if (sbjname.children.length > 0) {
+                        subjects.push({ "name": sbjname.firstElementChild.innerText.split(/\n/)[0], "teacher": teacher.innerText, "day": days.indexOf(day) + 1, "time": i });
+                    }
+                }
             }
         }
     }
 
+    console.log(subjects);
     var cals = [];
 
     let first = dayjs(start.slice(0,3) + "-" + start.slice(4,5) + "-" + start.slice(6,7) + 'T00:00:00.000Z');
@@ -50,18 +58,24 @@
         if (start_diff < 0) { day_diff += 7; }
 
         let cls_first_date = first.add(start_diff, 'date');
-
-        let end_diff = i - last.day();
-        if (end_diff > 0) { end_diff -= 7; }
-
-        let cls_last_date = end.add(end_diff, 'date');
+        let count = Math.floor(last.diff(cls_first_date, 'day') / 7);
 
         for (let s of subjects.filter(e => e.day == i)) {
             let cls_first = cls_first_date.add(timetable[s.time].slice(0,1), 'hour').add(timetable[s["time"]].slice(2,3), 'minutes');
-            let cls_end = cls_first.add(classtime, 'minutes');
+            let cls_last = cls_first.add(classtime, 'minutes');
 
-            cals.push(`BEGIN:VEVENT\nDTSTART;TZID=Asia/Tokyo:${cls_first.toISOString().slice(0,-1)}\nDTEND;TZID=Asia/Tokyo;${cls_end.toISOString().slice(0,-1)}`)
+            cals.push(`
+                BEGIN:VEVENT
+                DTSTART;TZID=Asia/Tokyo:${cls_first.toISOString().slice(0,-1)}
+                DTEND;TZID=Asia/Tokyo;${cls_last.toISOString().slice(0,-1)}
+                RRULE;FREQ=WEEKLY;COUNT=${count};BYDAY=${days[s.day - 1].slice(0,1).toUpperCase()}
+                SUMMARY:${s.name}
+                DESCRIPTION:${s.teacher}
+                END:VEVENT
+            `)
         }
     }
+
+    console.log(cals);
 
 })();
