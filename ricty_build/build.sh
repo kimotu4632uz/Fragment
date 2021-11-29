7c7f3f8a08161b02e4840e28f3cc0c6d22600340
kimotu4632uz <32472396+kimotu4632uz@users.noreply.github.com>
2021-11-29 22:12:57 +0900

    improbe Ricty generate scripts

--- ricty_build/build.sh
+++ ricty_build/build.sh
@@ -3,8 +3,10 @@
 migu_ver="migu-1m-20200307"
 
 apt update
-apt install -y curl unzip git fontforge python3-fontforge
-ln -s /usr/bin/python3 /usr/bin/python
+apt install -y curl unzip git fontforge python3-fontforge python-is-python3
+
+mkdir build
+cd ./build
 
 curl -LO https://rictyfonts.github.io/files/ricty_generator.sh
 curl -LO https://github.com/google/fonts/raw/main/ofl/inconsolata/static/Inconsolata-Regular.ttf
@@ -17,10 +19,12 @@ mv "${migu_ver}/migu-1m-bold.ttf" .
 mv "${migu_ver}/migu-1m-regular.ttf" .
 
 chmod +x ricty_generator.sh
-./ricty_generator.sh auto
+patch ricty_generator.sh < ../ricty_normal_em.patch
+./ricty_generator.sh -a auto
 
 ./nerd-fonts/font-patcher -c -l -w --careful Ricty-Regular.ttf
-./nerd-fonts/font-patcher -c -l -w --careful Ricty-Oblique.ttf
 
+mv 'Ricty Regular Nerd Font Complete Windows Compatible.ttf' ../
+cd ..
 ./rename.py
 
--- ricty_build/rename.py
+++ ricty_build/rename.py
@@ -1,16 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import fontforge
 
-f = fontforge.open('Ricty Regular Nerd Font Complete Windows Compatible.ttf')
-f.familyname = "RictyNF"
-f.fullname = "RictyNF Regular"
-f.fontname = "RictyNF-Regular"
-f.close()
+print('start rename')
 
-f = fontforge.open('Ricty Oblique Nerd Font Complete Windows Compatible.ttf')
-f.familyname = "RictyNF"
-f.fullname = "RictyNF Oblique"
-f.fontname = "RictyNF-Oblique"
+src = 'Ricty Regular Nerd Font Complete Windows Compatible.ttf'
+dst = 'RictyNF-Regular.ttf'
+
+f = fontforge.open(src)
+family = 'RictyNF'
+fullname = 'RictyNF Regular'
+fontname = 'RictyNF-Regular'
+
+f.familyname = family
+f.fullname = fullname
+f.fontname = fontname
+f.appendSFNTName('English (US)', 'PostScriptName', fontname)
+f.appendSFNTName('English (US)', 'Fullname', fullname)
+f.appendSFNTName('English (US)', 'Family', family)
+f.appendSFNTName('English (US)', 'Preferred Family', family)
+f.appendSFNTName('English (US)', 'Compatible Full', fullname)
+
+f.generate(dst, flags=('opentype', 'PfEd-comments'))
 f.close()
 
